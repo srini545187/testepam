5f2766c60d29dd24b2ed72d2000ab59cd81a77f7
venkatasrinivasrao <srrinivasrao@gmail.com>
2018-05-11 04:03:54 +0530

    updated TestUtil.java

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -75,7 +75,7 @@ public class TestUtil extends TestBase{
 		File source = ts.getScreenshotAs(OutputType.FILE);
 		// after execution, you could see a folder "FailedTestsScreenshots"
 		// under src folder
-		String destination = System.getProperty("user.dir") + "/FailedTestsScreenshots/" + screenshotName + dateName
+		String destination = System.getProperty("user.dir") + "/test-out/"+"/FailedTestsScreenshots/" + screenshotName + dateName
 				+ ".png";
 		File finalDestination = new File(destination);
 		FileUtils.copyFile(source, finalDestination);
