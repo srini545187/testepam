233274c35e511174b8480122aa9271d2d4a0f911
Srinivas Mamillapelli <Srinivas_Mamillapelli@epam.com>
2018-12-01 13:50:45 +0530

    changed Testbase class

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -45,7 +45,7 @@ public class TestBase {
 		String browserName = prop.getProperty("browser");
 		
 		if(browserName.equals("chrome")){
-			System.setProperty("webdriver.chrome.driver", "C:\\Software\\chromedriver_win32\\chromedriver.exe");	
+			System.setProperty("webdriver.chrome.driver", "C:\\software\\chromedriver_win32\\chromedriver.exe");	
 			driver = new ChromeDriver(); 
 		}
 		/*else if(browserName.equals("FF")){
